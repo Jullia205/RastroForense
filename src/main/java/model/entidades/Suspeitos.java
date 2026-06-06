3d799df838e4e16c502d79a6a9d949e5970fd677
Júlia Gabriella <110779825+Jullia205@users.noreply.github.com>
2026-06-06 15:17:04 -0300

    Revert "Finalização de entidades e estruturas (#2)" (#3)
    This reverts commit 5a354bceb19074af003cb0d2cf78618b55e8f249.

--- src/main/java/model/entidades/Suspeitos.java
+++ src/main/java/model/entidades/Suspeitos.java
@@ -1,17 +1,8 @@
 package model.entidades;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
-
-@Getter
-@Setter
-@AllArgsConstructor
-@ToString
 public class Suspeitos {
-    private String nome;
-    private int idade;
-    private String profissao;
-    public record Relacionamento(Suspeitos s1, Suspeitos s2, String relacao) {}
+    String nome;
+    int idade;
+    String profissao;
+    String relacionamentos;
 }
