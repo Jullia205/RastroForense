ec2a1b09c405d15d24ae2e0498d70be584b3a31d
Júlia Gabriella <110779825+Jullia205@users.noreply.github.com>
2026-06-06 15:16:49 -0300

    Revert "Finalização de entidades e estruturas (#2)"
    This reverts commit 5a354bceb19074af003cb0d2cf78618b55e8f249.

--- src/main/java/model/entidades/Ocorrencia.java
+++ src/main/java/model/entidades/Ocorrencia.java
@@ -1,16 +1,6 @@
 package model.entidades;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
-
-@Getter
-@Setter
-@AllArgsConstructor
-@ToString
 public class Ocorrencia {
-     private String descricao;
-     private String hora;
-     private Suspeitos quemRelatou;
+     public String descricao;
+     public String hora;
 }
--- src/main/java/model/ferramentas/NoOcorrencia.java
+++ src/main/java/model/ferramentas/NoOcorrencia.java
@@ -4,11 +4,9 @@ import model.entidades.Ocorrencia;
 public class NoOcorrencia {
     public Ocorrencia ocorrencia;
     public NoOcorrencia proxima;
-    public NoOcorrencia anterior;
 
     public NoOcorrencia(Ocorrencia ocorrencia){
         this.ocorrencia = ocorrencia;
         this.proxima = null;
-        this.anterior = null;
     }
 }
