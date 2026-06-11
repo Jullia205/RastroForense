f3be675dfdca5628797dc6955bf5a0436260c6f1
Julia <juliacostapereira.jgcp@gmail.com>
2026-06-11 14:40:55 -0300

    ACrrigind erros

---
+++ src/main/java/controller/Launcher.java
@@ -0,0 +1,9 @@
+package controller;
+
+// ESSA RODA O JOGO COM A INTERFACE JAVAFX
+
+public class Launcher {
+    public static void main(String[] args) {
+        Main.main(args);
+    }
+}
--- src/main/java/controller/Main.java
+++ src/main/java/controller/Main.java
@@ -6,7 +6,6 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-// ESSA RODA O JAVAFX
 
 public class Main extends Application {
 
